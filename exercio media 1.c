51627658346842c39e32249ce71bd3794574092c
Nego2K <44330434+Nego2K@users.noreply.github.com>
2019-08-19 13:54:48 -0300

    Add files via upload

---
+++ exercio media 1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	
+	int A,B;
+	float media;
+	
+	printf("Escreva dois numeros inteiros:\n");
+	scanf("%i %i", &A, &B);
+	
+	if(A!= 0 && B!=0){
+		media = (A+B)/2.0;
+		
+		printf("Media: %4.f", media);
+	}
+	else{
+		printf("Digite dois numeros diferentes de 0\n");
+	}
+	
+	return 0;
+}
