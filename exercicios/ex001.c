398644a0be00f55b22f7ea31a27ee4ec3575ec77
Nego2K <44330434+Nego2K@users.noreply.github.com>
2019-09-30 14:12:50 -0300

    Create ex001.c

---
+++ exercicios/ex001.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	
+	int n, i;
+	
+	for(n = 1;n<=10;n++){
+		
+		printf("1 X o numero %i\n", n);
+		
+		for(i=1;i<=n; i++){
+			
+			printf("%i\n", n);
+			
+		}
+	}
+	
+	return 0;
+}
